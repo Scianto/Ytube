091c23ece4e8d5189120bde860ef476677c1b1af
sci anto <65482441+Scianto@users.noreply.github.com>
2020-06-27 13:08:40 +0530

    Add files via upload

---
+++ Ytube.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Ytube v 1.1
+#this tool is mainly for downloading youtube videos in all format
+#author : sci_anto
+#github : https://github.com/Scianto/Ytube
+
+printf "\e[36m  ██  ██ ████████ ██    ██ ██████  ███████  \e[0m\n"
+printf "\e[36m  ██  ██    ██    ██    ██ ██   ██  ██       \e[0m\n"
+printf "\e[36m   ████        ██        ██    ██  ██████   █████      \e[0m\n"
+printf "\e[36m     ██           ██       ██    ██  ██     ██ ██         \e[0m\n"
+printf "\e[36m     ██          ██       ██████  ██████   ███████  \e[0m\n"
+
+printf "\e[32m         Created by Sci_anto         \e[0m\n"
+
+printf "\e[34m Instructions: You can download youtube videos in all format with this tool \e[34m\n"
+printf "\e[34m               copy and paste the youtube URL   ex:opYAyx1Humc       \e[34m\n"
+
+read -p $'\e[1;92m[\e[0m\e[1;77m?\e[0m\e[1;92m] Place the Youtube URL:\e[0m ' URL
+
+printf "\e[1;92m Your Youtube URL:\e[0m https://www.y2mate.com/youtube///%s\n" $URL
+
+printf "\e[1;37;1;41m Enjoy Watching *_* \e[0m\n"
