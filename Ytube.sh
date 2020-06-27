0cee885470070a1b39a41e0908e417d6175e3f16
sci anto <65482441+Scianto@users.noreply.github.com>
2020-06-27 13:16:14 +0530

    Update Ytube.sh

--- Ytube.sh
+++ Ytube.sh
@@ -10,7 +10,7 @@ printf "\e[36m    ██      ██    ██    ██  ██   ██ 
 printf "\e[36m    ██      ██     ██████   ██████   ███████  \e[0m\n"
 printf "\e[32m         Created by Sci_anto         \e[0m\n"
 printf "\e[34m Instructions: You can download youtube videos in all format with this tool \e[34m\n"
-printf "\e[34m               copy and paste the youtube URL   ex:opYAyx1Humc       \e[34m\n"
-read -p $'\e[1;92m[\e[0m\e[1;77m?\e[0m\e[1;92m] Place the Youtube URL:\e[0m ' URL
-printf "\e[1;92m Your Youtube URL:\e[0m https://www.y2mate.com/youtube///%s\n" $URL
+printf "\e[34m copy and paste the youtube URL   ex:opYAyx1Humc       \e[34m\n"
+read -p $'\e[1;92m[\e[0m\e[1;77m?\e[0m\e[1;92m] Place the Youtube url:\e[0m ' url
+printf "\e[1;92m Your Youtube URL:\e[0m https://www.y2mate.com/youtube///%s\n" $url
 printf "\e[1;37;1;41m Enjoy Watching *_* \e[0m\n"
